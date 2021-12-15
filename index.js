a27447524b2a5ac5a8f5c1e4ed69a8fae31cfbed
Luke Fowler <luciusfow@gmail.com>
2021-12-14 20:47:17 -0500

    commit tree 5530b1ec54b90e12d4c2fbd4477293cb43761e67

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+// superbowlWin = (record) => {
+//     const isWin = record.find( record => record.isWin === "W")
+// return !!isWin ? isWin.year : undefined
+// }
+function superbowlWin(record){
+    let isWin = record.find(record => record.result === "W")
+    if(isWin){
+    return isWin.year
+    }else {
+        return 
+    }
+}
